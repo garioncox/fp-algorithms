3624873b65b585a181a1a706697b5b8b27e39081
Garion Cox <garion.cox@students.snow.edu>
2023-12-04 15:25:58 -0700

    Added constraints. Thinking that WFC does not undo propagate when backtracking

--- src/main/kotlin/org/garioncox/WaveFunctionCollapse.kt
+++ src/main/kotlin/org/garioncox/WaveFunctionCollapse.kt
@@ -4,143 +4,174 @@ import java.lang.IllegalStateException
 import kotlin.math.min
 import kotlin.random.Random
 
-class WFC {
-    companion object {
-        fun toCells(board: Array<Array<Int>>): Array<Array<Cell>> {
-            val dim = board.size
-            val cellsToReturn = Array(board.size) { Array<Cell>(board[0].size) { Cell(BooleanArray(dim) { true }.toTypedArray()) } }
+class WFC(val constraints: IConstraints) {
+    fun toCells(board: Array<Array<Int>>): Array<Array<Cell>> {
+        val dim = board.size
+        val cellsToReturn =
+            Array(board.size) { Array<Cell>(board[0].size) { Cell(BooleanArray(dim) { true }.toTypedArray()) } }
 
-            for (row in 0..<dim) {
-                for (col in 0..<dim) {
+        for (row in 0..<dim) {
+            for (col in 0..<dim) {
 
-                    // Get Neighbors
-                    val current = board[row][col]
-                    val neighbors = board.map { it[col] } + board[row] - current
+                // Get Neighbors
+                val current = board[row][col]
 
-                    val domain: BooleanArray
-                    val isSolved: Boolean
-                    // If current is not solved, set domain to all possible values
-                    if (current == 0) {
-                        domain = BooleanArray(dim) { true }
-                        isSolved = false
-                    }
-                    // If current is solved, set the domain to the solved value
-                    else {
-                        domain = BooleanArray(dim) { false }
-                        domain[current - 1] = true
-                        isSolved = true
-                    }
-
-                    cellsToReturn[row][col] = Cell(domain.toTypedArray(), isSolved)
+                val domain: BooleanArray
+                val isSolved: Boolean
+                // If current is not solved, set domain to all possible values
+                if (current == 0) {
+                    domain = BooleanArray(dim) { true }
+                    isSolved = false
                 }
-            }
+                // If current is solved, set the domain to the solved value
+                else {
+                    domain = BooleanArray(dim) { false }
+                    domain[current - 1] = true
+                    isSolved = true
+                }
 
-            // Update the domain of all cells
-            return propagate(cellsToReturn)
+                cellsToReturn[row][col] = Cell(domain.toTypedArray(), isSolved)
+            }
         }
 
-        fun propagate(board: Array<Array<Cell>>): Array<Array<Cell>> {
-            val dim = board.size
+        // Update the domain of all cells
+        return propagate(cellsToReturn)
+    }
 
-            for (row in 0..< dim) {
-                for (col in 0..< dim) {
-                    val current = board[row][col]
-                    if (current.isSolved) { continue }
+    fun propagate(board: Array<Array<Cell>>): Array<Array<Cell>> {
+        val dim = board.size
 
-                    val neighbors = board.map { it[col] } + board[row] - current
-//                    val neighbors = getNeighbors(row, col, board)
-                    for (n in neighbors) {
-                        if (!n.isSolved) { continue }
-
-                        // Remove values in domain that appear in solved neighbors
-                        current.domain[n.domain.indexOf(true)] = false
-                    }
-
-                    // If the cell has only one value in its domain, set it to solved
-                    if (current.domain.count { it } == 1) {
-                        current.isSolved = true
-                    }
-
-                    // If the cell has no valid domain, throw exception
-                    if (current.domain.count { it } == 0) {
-                        throw IllegalStateException()
-                    }
-                }
-            }
-
-            return board
-        }
-
-        fun getNeighbors(row: Int, col: Int, board: Array<Array<Cell>>): Collection<Cell> {
-            val currentCell = board[row][col]
-            val rowCells = board[row].toSet()
-            val colCells = board.map { it[col] }.toSet()
-            val boxCells = getBoxCells(row, col, board)
-
-            return colCells + rowCells + boxCells - currentCell
-        }
-
-        fun getBoxCells(row: Int, col: Int, board: Array<Array<Cell>>): Set<Cell> {
-            val box = mutableSetOf<Cell>()
-            for (i in 0..< 3) {
-                for (j in 0..< 3) {
-                    box.add(board[(row/3)*3 + i][(col/3)*3 + j])
-                }
-            }
-
-            return box
-        }
-
-        fun guess(board: Array<Array<Cell>>): Array<Array<Cell>> {
-
-            val randomCell: Cell
-            try {
-                randomCell = getRandomCellWithLeastEntropy(board)
-            }
-            catch (e: IllegalStateException) {
-                return board
-            }
-
-            var toReturn: Array<Array<Cell>>
-            while (true) {
-                val randomCellDomain = randomCell.domain.indices.filter { randomCell.domain[it] }
-                val updatedDomain = BooleanArray(board.size) { false }
-                val choice = randomCellDomain.random()
-                updatedDomain[choice] = true
-
-                randomCell.domain = updatedDomain.toTypedArray()
-                randomCell.isSolved = true
-
-                try {
-                   toReturn = propagate(board)
-                }
-                catch (e: IllegalStateException) {
-                    // If failed to propagate, remove the choice from the options and try again
-                    randomCell.domain[choice] = false
+        for (row in 0..<dim) {
+            for (col in 0..<dim) {
+                val current = board[row][col]
+                if (current.isSolved) {
                     continue
                 }
 
-                break
+//                val neighbors = board.map { it[col] } + board[row] - current
+                val neighbors = constraints.getNeighborsOf(row, col, board)
+                for (n in neighbors) {
+                    if (!n.isSolved) {
+                        continue
+                    }
+
+                    // Remove values in domain that appear in solved neighbors
+                    current.domain[n.domain.indexOf(true)] = false
+                }
+
+                // If the cell has only one value in its domain, set it to solved
+                if (current.domain.count { it } == 1) {
+                    current.isSolved = true
+                }
+
+                // If the cell has no valid domain, throw exception
+                if (current.domain.count { it } == 0) {
+                    throw IllegalStateException()
+                }
+            }
+        }
+
+        return board
+    }
+
+    fun guess(board: Array<Array<Cell>>): Array<Array<Cell>> {
+
+        val randomCell: Cell
+        try {
+            randomCell = getRandomCellWithLeastEntropy(board)
+        } catch (e: IllegalStateException) {
+            return board
+        }
+
+        var toReturn: Array<Array<Cell>>
+        while (true) {
+            val randomCellDomain = randomCell.domain.copyOf()
+            val validRandomCellDomain = randomCell.domain.indices.filter { randomCell.domain[it] }
+            val updatedDomain = BooleanArray(board.size) { false }
+            val choice = validRandomCellDomain.random()
+            updatedDomain[choice] = true
+
+            randomCell.domain = updatedDomain.toTypedArray()
+            randomCell.isSolved = true
+
+            try {
+                toReturn = propagate(board)
+            } catch (e: IllegalStateException) {
+                // If failed to propagate, remove the choice from the options and try again
+                randomCell.domain = randomCellDomain
+                randomCell.domain[choice] = false
+                randomCell.isSolved = false
+
+                // If there is only one value in the domain, that is the solution
+                if (randomCell.domain.indices.count { randomCell.domain[it] } == 1) {
+                    toReturn = board
+                    break
+                }
+
+                continue
             }
 
-            return toReturn
+            break
         }
 
-        private fun getRandomCellWithLeastEntropy(board: Array<Array<Cell>>): Cell {
-            val unsolvedCells = board.flatten().filter { cell -> !cell.isSolved }
-            if (unsolvedCells.isEmpty()) { throw IllegalStateException() }
+        return toReturn
+    }
 
-            val leastEntropy = unsolvedCells.minOf {cell -> cell.domain.count { it } }
-            if (leastEntropy == 1) { throw IllegalStateException() } // Should never be 1. This means all cells are solved
-
-            val cellsWithLeastEntropy = unsolvedCells.filter { cell -> cell.domain.count { it } == leastEntropy }
-            return cellsWithLeastEntropy.random()
+    fun isSolved(board: Array<Array<Cell>>): Boolean {
+        for (row in 0..< board.size) {
+            for (col in 0..< board[row].size) {
+                if (!board[row][col].isSolved) { return false }
+            }
         }
+
+        return true
+    }
+
+    private fun getRandomCellWithLeastEntropy(board: Array<Array<Cell>>): Cell {
+        val unsolvedCells = board.flatten().filter { cell -> !cell.isSolved }
+        if (unsolvedCells.isEmpty()) {
+            throw IllegalStateException()
+        }
+
+        val leastEntropy = unsolvedCells.minOf { cell -> cell.domain.count { it } }
+        if (leastEntropy == 1) {
+            throw IllegalStateException()
+        } // Should never be 1. This means all cells are solved
+
+        val cellsWithLeastEntropy = unsolvedCells.filter { cell -> cell.domain.count { it } == leastEntropy }
+        return cellsWithLeastEntropy.random()
     }
 }
+
 
 class Cell(var domain: Array<Boolean>, var isSolved: Boolean = false) {
     fun isEqualTo(expected: Cell): Boolean {
         return expected.domain.contentEquals(this.domain) && expected.isSolved == this.isSolved
+    }
+}
+
+interface IConstraints {
+    fun getNeighborsOf(row: Int, col: Int, board: Array<Array<Cell>>): Collection<Cell>
+}
+
+class SudokuConstraints : IConstraints {
+    override fun getNeighborsOf(row: Int, col: Int, board: Array<Array<Cell>>): Collection<Cell> {
+        val currentCell = board[row][col]
+        val rowCells = board[row].toSet()
+        val colCells = board.map { it[col] }.toSet()
+        val boxCells = getBoxCells(row, col, board)
+
+        return colCells + rowCells + boxCells - currentCell
+    }
+
+    private fun getBoxCells(row: Int, col: Int, board: Array<Array<Cell>>): Set<Cell> {
+        val box = mutableSetOf<Cell>()
+        for (i in 0..<3) {
+            for (j in 0..<3) {
+                box.add(board[(row / 3) * 3 + i][(col / 3) * 3 + j])
+            }
+        }
+
+        return box
     }
 }
--- src/test/kotlin/org/garioncox/Tests.kt
+++ src/test/kotlin/org/garioncox/Tests.kt
@@ -1,10 +1,17 @@
 package org.garioncox
 
+import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 
 class Tests {
+    private lateinit var wfc: WFC
+    @BeforeTest
+    fun before() {
+        wfc = WFC(TestConstraints())
+    }
     @Test
     fun `Convert Initial Array to Cells`() {
         val board = arrayOf(
@@ -12,7 +19,7 @@ class Tests {
             arrayOf(0, 0)
         )
 
-        val cells = WFC.toCells(board)
+        val cells = wfc.toCells(board)
 
         for (row in cells) {
             for (cell in row) {
@@ -29,7 +36,7 @@ class Tests {
             arrayOf(0, 0, 0)
         )
 
-        val cells = WFC.toCells(board)
+        val cells = wfc.toCells(board)
 
         val expected = arrayOf(
             arrayOf(Cell(arrayOf(false, true, true)), Cell(arrayOf(false, true, true)), Cell(arrayOf(true, false, false), true)),
@@ -48,11 +55,11 @@ class Tests {
             arrayOf(0, 0, 0)
         )
 
-        val cells = WFC.toCells(board)
+        val cells = wfc.toCells(board)
 
         // Make a guess
         cells[0][2] = Cell(arrayOf(true, false, false), true)
-        WFC.propagate(cells)
+        wfc.propagate(cells)
 
         val expected = arrayOf(
             arrayOf(Cell(arrayOf(false, true, true)), Cell(arrayOf(false, true, true)), Cell(arrayOf(true, false, false), true)),
@@ -71,14 +78,14 @@ class Tests {
             arrayOf(0, 0, 0)
         )
 
-        val cells = WFC.toCells(board)
+        val cells = wfc.toCells(board)
 
         // Minimal guesses to solve board
         cells[0][0] = Cell(arrayOf(false, true, false), true)
         cells[0][2] = Cell(arrayOf(true, false, false), true)
         cells[1][1] = Cell(arrayOf(true, false, false), true)
         cells[0][2] = Cell(arrayOf(true, false, false), true)
-        WFC.propagate(cells)
+        wfc.propagate(cells)
 
         val expected = arrayOf(
             arrayOf(Cell(arrayOf(false, true, false), true), Cell(arrayOf(false, false, true), true), Cell(arrayOf(true, false, false), true)),
@@ -97,11 +104,11 @@ class Tests {
             arrayOf(0, 0, 0)
         )
 
-        val cells = WFC.toCells(board)
+        val cells = wfc.toCells(board)
 
         for (i in 0..2) {
-            WFC.guess(cells)
-            WFC.propagate(cells)
+            wfc.guess(cells)
+            wfc.propagate(cells)
         }
 
         for (i in 0..< cells.size) {
@@ -109,6 +116,111 @@ class Tests {
                 assertTrue(cells[i][j].isSolved)
             }
         }
+    }
+
+    @Test
+    fun `Sudoku Board Gets Neighbors with Constraints`() {
+        wfc = WFC(SudokuConstraints())
+
+        val input = ToSudokuCells(arrayOf(
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9),
+            arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        ))
+
+        val expected = arrayOf(
+            input[0][3], input[1][3], input[2][3], input[4][3], input[5][3], input[6][3], input[7][3], input[8][3],
+            input[3][0], input[3][1], input[3][2], input[3][4], input[3][5], input[3][6], input[3][7], input[3][8],
+            input[4][4], input[4][5], input[5][4], input[5][5]
+        )
+
+        val neighbors = SudokuConstraints().getNeighborsOf(3, 3, input).toMutableList()
+
+        for (i in 0..< neighbors.size) {
+            assertTrue(neighbors[i].isEqualTo(expected[i]))
+        }
+    }
+
+    @Test
+    fun `Sudoku Board Solves`() {
+        wfc = WFC(SudokuConstraints())
+
+        val input = arrayOf(
+            arrayOf(4, 8, 3, 9, 2, 1, 6, 5, 7),
+            arrayOf(9, 6, 7, 3, 4, 5, 8, 2, 1),
+            arrayOf(2, 5, 0, 8, 7, 6, 4, 9, 3),
+            arrayOf(5, 4, 8, 1, 3, 2, 9, 7, 6),
+            arrayOf(7, 2, 9, 5, 6, 4, 1, 3, 8),
+            arrayOf(1, 3, 6, 7, 9, 8, 0, 4, 5),
+            arrayOf(3, 7, 2, 6, 8, 9, 5, 1, 4),
+            arrayOf(8, 1, 4, 0, 5, 3, 7, 6, 9),
+            arrayOf(6, 9, 5, 4, 1, 7, 3, 8, 2)
+        )
+
+        val expected = ToSudokuCells(arrayOf(
+            arrayOf(4, 8, 3, 9, 2, 1, 6, 5, 7),
+            arrayOf(9, 6, 7, 3, 4, 5, 8, 2, 1),
+            arrayOf(2, 5, 1, 8, 7, 6, 4, 9, 3),
+            arrayOf(5, 4, 8, 1, 3, 2, 9, 7, 6),
+            arrayOf(7, 2, 9, 5, 6, 4, 1, 3, 8),
+            arrayOf(1, 3, 6, 7, 9, 8, 2, 4, 5),
+            arrayOf(3, 7, 2, 6, 8, 9, 5, 1, 4),
+            arrayOf(8, 1, 4, 2, 5, 3, 7, 6, 9),
+            arrayOf(6, 9, 5, 4, 1, 7, 3, 8, 2)
+        ))
+
+        val cells = wfc.toCells(input)
+
+        for (i in 0..2) {
+            wfc.guess(cells)
+            wfc.propagate(cells)
+        }
+
+        cells.isEqualTo(expected)
+    }
+
+    @Test
+    fun `Sudoku Board Solves 2`() {
+        wfc = WFC(SudokuConstraints())
+
+        val input = arrayOf(
+            arrayOf(0, 0, 3, 0, 2, 0, 6, 0, 0),
+            arrayOf(9, 0, 0, 3, 0, 5, 0, 0, 1),
+            arrayOf(0, 0, 1, 8, 0, 6, 4, 0, 0),
+            arrayOf(0, 0, 8, 1, 0, 2, 9, 0, 0),
+            arrayOf(7, 0, 0, 0, 0, 0, 0, 0, 8),
+            arrayOf(0, 0, 6, 7, 0, 8, 2, 0, 0),
+            arrayOf(0, 0, 2, 6, 0, 9, 5, 0, 0),
+            arrayOf(8, 0, 0, 2, 0, 3, 0, 0, 9),
+            arrayOf(0, 0, 5, 0, 1, 0, 3, 0, 0)
+        )
+
+        val expected = ToSudokuCells(arrayOf(
+            arrayOf(4, 8, 3, 9, 2, 1, 6, 5, 7),
+            arrayOf(9, 6, 7, 3, 4, 5, 8, 2, 1),
+            arrayOf(2, 5, 1, 8, 7, 6, 4, 9, 3),
+            arrayOf(5, 4, 8, 1, 3, 2, 9, 7, 6),
+            arrayOf(7, 2, 9, 5, 6, 4, 1, 3, 8),
+            arrayOf(1, 3, 6, 7, 9, 8, 2, 4, 5),
+            arrayOf(3, 7, 2, 6, 8, 9, 5, 1, 4),
+            arrayOf(8, 1, 4, 2, 5, 3, 7, 6, 9),
+            arrayOf(6, 9, 5, 4, 1, 7, 3, 8, 2)
+        ))
+
+        val cells = wfc.toCells(input)
+
+        while (!wfc.isSolved(cells)) {
+            wfc.guess(cells)
+            //wfc.propagate(cells)
+        }
+
+        cells.isEqualTo(expected)
     }
 }
 
@@ -120,4 +232,47 @@ private fun Array<Array<Cell>>.isEqualTo(expected: Array<Array<Cell>>): Boolean 
     }
 
     return true
+}
+
+private fun ToSudokuCells(inputBoard: Array<Array<Int>>): Array<Array<Cell>> {
+    val one =   arrayOf(true, false, false, false, false, false, false, false, false)
+    val two =   arrayOf(false, true, false, false, false, false, false, false, false)
+    val three = arrayOf(false, false, true, false, false, false, false, false, false)
+    val four =  arrayOf(false, false, false, true, false, false, false, false, false)
+    val five =  arrayOf(false, false, false, false, true, false, false, false, false)
+    val six =   arrayOf(false, false, false, false, false, true, false, false, false)
+    val seven = arrayOf(false, false, false, false, false, false, true, false, false)
+    val eight = arrayOf(false, false, false, false, false, false, false, true, false)
+    val nine =  arrayOf(false, false, false, false, false, false, false, false, true)
+
+    val inputCells = Array(9) { Array(9) { Cell(one) } }
+
+    for (row in 0..< inputBoard.size) {
+        for (col in 0..< inputBoard[row].size) {
+            when (inputBoard[row][col]) {
+                1 -> inputCells[row][col] = Cell(one)
+                2 -> inputCells[row][col] = Cell(two)
+                3 -> inputCells[row][col] = Cell(three)
+                4 -> inputCells[row][col] = Cell(four)
+                5 -> inputCells[row][col] = Cell(five)
+                6 -> inputCells[row][col] = Cell(six)
+                7 -> inputCells[row][col] = Cell(seven)
+                8 -> inputCells[row][col] = Cell(eight)
+                9 -> inputCells[row][col] = Cell(nine)
+                else -> { inputCells[row][col] = Cell(arrayOf()) }
+            }
+        }
+    }
+
+    return inputCells
+}
+
+class TestConstraints : IConstraints {
+    override fun getNeighborsOf(row: Int, col: Int, board: Array<Array<Cell>>): Collection<Cell> {
+        val currentCell = board[row][col]
+        val rowCells = board[row].toSet()
+        val colCells = board.map { it[col] }.toSet()
+
+        return colCells + rowCells - currentCell
+    }
 }
